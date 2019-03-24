f908f1546d5fc58f57376ab10ffd18719c522619
KomaEc <31341411+KomaEc@users.noreply.github.com>
2019-03-24 18:06:55 +0800

    Update support.ml

--- support.ml
+++ support.ml
@@ -1,4 +1,4 @@
-open Format 
+open Printf
 
 module Error = struct 
 
@@ -10,30 +10,11 @@ type 'a withinfo = {i : info; v : 'a}
 let dummyinfo = UNKNOWN 
 let createInfo f i c = FI(f, i, c)
 
-let errf f = 
-  print_flush();
-  open_vbox 0;
-  open_hvbox 0; f(); print_cut(); close_box(); print_newline(); 
-  raise (Exit 1)
 
-let printInfo = function 
+let printInfo outx = function
   | FI (f, l, c) -> 
-    print_string f;
-    print_string " : ";
-    print_int l; print_string ".";
-    print_int c; print_string ":"
-  | UNKNOWN ->
-    print_string "<Unknown file and line>: "
+    fprintf outx "%s : line %d : offset %d:" f l c
+  | UNKNOWN -> 
+    fprintf outx "%s" "<Unknown file and line>: "
 
-let errfAt fi f = errf (fun () -> printInfo fi; print_space(); f())
-
-let err s = errf (fun () -> print_string "Error : "; print_string s; print_newline())
-
-let error fi s = errfAt fi (fun () -> print_string s; print_newline())
-
-let warning s =
-  print_string "Warning : "; print_string s; print_newline()
-
-let warningAt fi s = 
-  printInfo fi; warning s
 end
