981d1c189e0de3d880ba4542acb340552809e8a8
KomaEc <31341411+KomaEc@users.noreply.github.com>
2019-04-04 00:10:36 +0800

    Add init/def check (TODO: type check)

--- sement.ml
+++ sement.ml
@@ -2,6 +2,7 @@
 open Ast
 open Symbol
 open Support.Error
+open Translate
 
 exception Duplicated_Definition of info 
 exception Lack_Definition of info
@@ -13,27 +14,49 @@ exception Not_Function of info
 exception No_Fieldname of info
 exception Not_Struct of info
 
+module type env = sig 
+
+  type ty
+  (* struct env *)
+  val base_senv : (Symbol.t, ty) Hashtbl.t Symbol.table
+  (* variable env, maps vars to types *)
+  val base_venv : ty Symbol.table
+end
+
+module Env : env with type ty = Ast.ty = struct 
+  type ty = Ast.ty
+  let base_senv = Symbol.empty 
+  let base_venv = Symbol.empty
+end
+
 type def_bind = 
   | Vardef | Fundef of bool ref | Strucdef of bool ref * (Symbol.t, Ast.ty) Hashtbl.t
 
-let rec check_def def_env = function 
+type def_env = def_bind Symbol.table 
+type str_env = (Symbol.t, Ast.ty) Hashtbl.t Symbol.table
+
+(**  check function declaration and variable declaration 
+ **  create a global struct environment *)
+let rec check_def : def_env -> Ast.stmt -> str_env = fun def_env -> function 
   | If(e,s,sop,_) -> 
     check_def_exp def_env e;
-    check_def def_env s; 
-    (match sop with 
-     | Some s' -> check_def def_env s
-     | None -> ()) 
-  | While(e,s,i) -> check_def_exp def_env e; check_def def_env s 
-  | Seq(sl,_) -> List.iter (check_def def_env) sl 
+    ignore (check_def def_env s); 
+    ignore (match sop with 
+     | Some s' -> ignore (check_def def_env s)
+     | None -> ());
+    empty
+  | While(e,s,i) -> check_def_exp def_env e; ignore (check_def def_env s); empty
+  | Seq(sl,_) -> List.iter (fun s -> ignore (check_def def_env s)) sl;  empty
   | Vardecl(id,_,s,i) -> 
     (try check_id_def id i def_env
-    with Not_found -> check_def (enter id Vardef def_env) s)
+    with Not_found -> ignore (check_def (enter id Vardef def_env) s));
+    empty
   | Fundecl(id,_,s,i) -> 
-    (try check_id_def id i def_env
+    (try check_id_def id i def_env; empty (* can't be reached *)
     with Not_found -> 
       let br = ref false in 
-      check_def (enter id (Fundef(br)) def_env) s;
-      if !br then () else raise (Lack_Definition i)) 
+      (let res = check_def (enter id (Fundef(br)) def_env) s in
+      (if !br then () else raise (Lack_Definition i)); res))
   | Fundefn(id,idl,_,s',s,i) -> 
     (try let d = lookup id def_env in 
          match d with 
@@ -41,21 +64,23 @@ let rec check_def def_env = function
          | Strucdef(_) -> raise (Duplicated_Definition i)
          | Fundef br -> if !br then raise (Duplicated_Definition i)
                         else br := true;
-                             check_def 
+                             ignore (check_def 
                              (List.fold_left
                               (fun acc id -> enter id Vardef acc) def_env idl)
-                             s'; check_def def_env s
-    with Not_found -> check_def (List.fold_left (fun acc id -> enter id Vardef acc) def_env idl) s';
+                             s'); check_def def_env s
+    with Not_found -> ignore (check_def (List.fold_left (fun acc id -> enter id Vardef acc) def_env idl) s');
                       check_def (enter id (Fundef(ref true)) def_env) s) 
-  | Exp(e,_) -> check_def_exp def_env e
-  | Return(e,_) -> check_def_exp def_env e
-  | Assign(_,e,_) -> check_def_exp def_env e 
+  | Exp(e,_) -> check_def_exp def_env e; empty
+  | Return(e,_) -> check_def_exp def_env e; empty
+  | Assign(_,e,_) -> check_def_exp def_env e; empty 
   | Structdecl(id,s,i) -> 
-    (try check_id_def id i def_env
+    (try check_id_def id i def_env; empty (* can't be reached *)
     with Not_found -> 
       let br = ref false in 
-      check_def (enter id (Strucdef(br, Hashtbl.create 20)) def_env) s;
-      if !br then () else raise (Lack_Definition i))
+      let res = check_def (enter id (Strucdef(br, Hashtbl.create 20)) def_env) s in
+      (if !br then () else raise (Lack_Definition i));
+      res
+    )
   | Structdefn(id,fl,s,i) -> 
     (try let d = lookup id def_env in 
          match d with 
@@ -67,22 +92,22 @@ let rec check_def def_env = function
                                         (match t with 
                                         | NameTy(tid) -> (try ignore (lookup tid def_env) with Not_found -> raise (Lack_Definition i))
                                         | _ -> ()); Hashtbl.add tbl id t)
-                             fl; check_def def_env s
+                             fl; enter id tbl (check_def def_env s)
     with Not_found -> 
       let tbl = Hashtbl.create 20 in 
       List.iter (fun (id,t) -> 
                 (match t with
                 | NameTy(tid) -> (try ignore (lookup tid def_env) with Not_found -> raise(Lack_Definition i))
                 | _ -> ()); Hashtbl.add tbl id t)
-                fl; check_def (enter id (Strucdef(ref true, tbl)) def_env) s) 
-  | Nop -> ()
-and check_id_def id i def_env =
+                fl; enter id tbl (check_def (enter id (Strucdef(ref true, tbl)) def_env) s) )
+  | Nop -> empty
+and check_id_def id i def_env : unit =
   let _ = lookup id def_env in raise (Duplicated_Definition i)
 and check_id_isdef id i def_env = 
   (try let _ = lookup id def_env in () 
   with Not_found -> raise (Lack_Definition i))
 and check_def_exp def_env = function 
-  | Var(v) -> check_def_var_exp def_env (fun _ -> ()) v
+  | Var(v) -> check_def_var_exp def_env v
   | Bin(e1,_,e2,_) -> check_def_exp def_env e1; check_def_exp def_env e2
   | Un(_,e,_) -> check_def_exp def_env e
   | App(id,el,i) -> check_id_isdef id i def_env;
@@ -97,18 +122,12 @@ and check_def_exp def_env = function
     | _ -> ()) 
   | Intconst(_) | True(_) | False(_)
   | Nil -> ()
-and check_def_var_exp def_env k = function (* Not Correct! Must combine with Type Check *)
-  | SimpVar(id,i) -> k id; check_id_isdef id i def_env 
-  | FieldVar(v,fname,i) -> () (* TODO *);
-    check_def_var_exp def_env 
-    (fun id -> 
-    try (match lookup id def_env with 
-        | Strucdef(_,tbl) -> (match Hashtbl.find_opt tbl id with 
-                             | Some t -> () (* Enclosing Continuation *) 
-                             | None -> raise (No_Fieldname i))
-        | _ -> raise (Not_Struct i))
-    with Not_found -> raise (No_Fieldname i)) v
-  | _ -> ()
+and check_def_var_exp def_env = function 
+  | SimpVar(id,i) -> 
+    (try ignore (lookup id def_env)
+    with Not_found -> raise (Lack_Definition(i)))
+  | FieldVar(v,_,_) -> check_def_var_exp def_env v 
+  | SubscriptVar(v,_,_) -> check_def_var_exp def_env v
 
 (* TODO: add init check for struct *)
 let rec check_init = function 
