b6cebfcf159a51e6ba78730639f8a26952a40160
KomaEc <31341411+KomaEc@users.noreply.github.com>
2019-04-04 15:37:34 +0800

    Add Type Check

--- sement.ml
+++ sement.ml
@@ -1,5 +1,6 @@
 
 open Ast
+open Ast.Util
 open Symbol
 open Support.Error
 open Translate
@@ -13,10 +14,13 @@ exception Fundec_Mismatched of info
 exception Not_Function of info
 exception No_Fieldname of info
 exception Not_Struct of info
+exception Alloc_Non_Struct of info
+exception Type_Var_Misuse of info
 
 module type env = sig 
 
   type ty
+  type entry = Var of ty | Func of ty list * ty
   (* struct env *)
   val base_senv : (Symbol.t, ty) Hashtbl.t Symbol.table
   (* variable env, maps vars to types *)
@@ -25,6 +29,7 @@ end
 
 module Env : env with type ty = Ast.ty = struct 
   type ty = Ast.ty
+  type entry = Var of ty | Func of ty list * ty
   let base_senv = Symbol.empty 
   let base_venv = Symbol.empty
 end
@@ -34,6 +39,7 @@ type def_bind =
 
 type def_env = def_bind Symbol.table 
 type str_env = (Symbol.t, Ast.ty) Hashtbl.t Symbol.table
+type var_env = Env.entry Symbol.table 
 
 (**  check function declaration and variable declaration 
  **  create a global struct environment *)
@@ -72,7 +78,7 @@ let rec check_def : def_env -> Ast.stmt -> str_env = fun def_env -> function
                       check_def (enter id (Fundef(ref true)) def_env) s) 
   | Exp(e,_) -> check_def_exp def_env e; empty
   | Return(e,_) -> check_def_exp def_env e; empty
-  | Assign(_,e,_) -> check_def_exp def_env e; empty 
+  | Assign(var,e,_) -> check_def_var_exp def_env var; check_def_exp def_env e; empty 
   | Structdecl(id,s,i) -> 
     (try check_id_def id i def_env; empty (* can't be reached *)
     with Not_found -> 
@@ -124,7 +130,9 @@ and check_def_exp def_env = function
   | Nil -> ()
 and check_def_var_exp def_env = function 
   | SimpVar(id,i) -> 
-    (try ignore (lookup id def_env)
+   (try (match lookup id def_env with 
+        | Strucdef(_) -> raise (Type_Var_Misuse i)
+        | _ -> ())
     with Not_found -> raise (Lack_Definition(i)))
   | FieldVar(v,_,_) -> check_def_var_exp def_env v 
   | SubscriptVar(v,_,_) -> check_def_var_exp def_env v
@@ -182,79 +190,177 @@ and def id = function
   | _ -> false
 
 
-let rec check_type ctx = function
-  | Assign(SimpVar(id,_),e,i) ->
-    let ty = lookup id ctx in 
-    let ty' = check_type_exp ctx e in 
-    if not (ty = ty') then raise (Ill_Typed i) else () 
-  | If(e,s,sop,i) -> 
-    let ty = check_type_exp ctx e in 
-    if not (ty = Bool) then raise(Ill_Typed (extract_info_exp e))
-    else check_type ctx s; 
-    (match sop with Some s' -> check_type ctx s' | None -> ())
-  | While(e,s,i) -> 
-    let ty = check_type_exp ctx e in 
-    if not (ty = Bool) then raise(Ill_Typed (extract_info_exp e)) 
-    else check_type ctx s 
-  | Seq(sl,_) -> List.iter (check_type ctx) sl 
-  | Vardecl(id,t,s,_) -> check_type (enter id t ctx) s 
-  | Fundecl(id,t,s,_) -> check_type (enter id t ctx) s 
-  | Fundefn(id,idl,t,s',s,i) -> 
-    (match t with 
-    | Arrow(tyl, ty) as t -> 
-      (try let t' = lookup id ctx in 
-      if not (t = t') then raise (Fundec_Mismatched i) with 
-      Not_found -> ());
-      if not (List.length idl = List.length tyl) then raise (Arity_Mismatched i)
-      else let ctx' = enter id t ctx in 
-           let ctx'' = List.fold_left2 
-           (fun acc id ty -> enter id ty acc) ctx' idl tyl in 
-           check_type ctx'' s';
-           check_type ctx' s 
-    | _ -> assert false)
-  | _ -> ()
-and check_type_exp ctx = function 
-  | Intconst(_) -> Int 
-  | True(_) | False(_) -> Bool 
-  | Var(SimpVar(id,_)) -> lookup id ctx
-  | Bin(e1,binop,e2,i) ->
-    (match binop with 
-    | Plus | Minus | Times | Div -> 
-      check_int_binop ctx e1 e2 i; Int 
-    | Lt | Gt -> 
-      check_int_binop ctx e1 e2 i; Bool
-    | And | Or -> check_bool_binop ctx e1 e2 i; Bool 
-    | Eq -> 
-      let ty = check_type_exp ctx e1 in 
-      let ty' = check_type_exp ctx e2 in 
-      if not (ty = ty') then raise (Ill_Typed(i))
-      else ty
-      )
-  | Un(uop,e,i) -> 
-    let ty = check_type_exp ctx e in 
-    if not (ty = Bool) then raise (Ill_Typed(i))
-    else Bool 
-  | App(id,el,i) -> 
-    (match lookup id ctx with 
-    | Arrow(tyl, ty) -> 
-      if not (List.length tyl = List.length el) 
-      then raise (Arity_Mismatched(i))
-      else List.iter2 (fun ty e -> if not (ty = check_type_exp ctx e) 
-                                   then raise (Ill_Typed(extract_info_exp e))
-                                   else ()) tyl el; ty 
-    | _ -> raise (Not_Function(i)))
-and check_int_binop ctx e1 e2 i = 
-  let ty = check_type_exp ctx e1 in 
-  let ty' = check_type_exp ctx e2 in 
-  if not (ty = Int && ty' = Int) then raise (Ill_Typed(i))
-  else ()
-and check_bool_binop ctx e1 e2 i = 
-  let ty = check_type_exp ctx e1 in 
-  let ty' = check_type_exp ctx e2 in 
-  if not (ty = Bool && ty' = Bool) then raise (Ill_Typed(i))
-  else ()
+
+type exp_and_ty = { exp : Translate.exp; ty : Ast.ty }
+
+let trans_exp : var_env -> str_env -> Ast.exp -> exp_and_ty = 
+  fun venv glb_senv -> 
+  let check_int : exp_and_ty -> info -> unit = 
+    fun { exp; ty } i -> 
+      (match ty with 
+       | Ast.Int -> () 
+       | _ -> raise (Ill_Typed i)) in
+  let check_bool : exp_and_ty -> info -> unit = 
+    fun { exp; ty } i -> 
+      (match ty with 
+       | Ast.Bool -> () 
+       | _ -> raise (Ill_Typed i)) in
+  let rec trexp = function 
+  | Intconst(num,_) -> { exp = (); ty = Ast.Int }
+  | True(_) -> { exp = (); ty = Ast.Bool }
+  | False(_) -> { exp = (); ty = Ast.Bool }
+  | Bin(e1, Plus, e2, i) ->
+    check_int (trexp e1) i;
+    check_int (trexp e2) i;
+    { exp = (); ty = Ast.Int }
+  | Bin(e1, Minus, e2, i) ->
+    check_int (trexp e1) i;
+    check_int (trexp e2) i;
+    { exp = (); ty = Ast.Int }
+  | Bin(e1, Times, e2, i) ->
+    check_int (trexp e1) i;
+    check_int (trexp e2) i;
+    { exp = (); ty = Ast.Int }
+  | Bin(e1, Div, e2, i) ->
+    check_int (trexp e1) i;
+    check_int (trexp e2) i;
+    { exp = (); ty = Ast.Int }
+  | Bin(e1, Lt, e2, i) ->
+    check_int (trexp e1) i;
+    check_int (trexp e2) i;
+    { exp = (); ty = Ast.Bool }
+  | Bin(e1, Gt, e2, i) ->
+    check_int (trexp e1) i;
+    check_int (trexp e2) i;
+    { exp = (); ty = Ast.Bool }
+  | Bin(e1, And, e2, i) ->
+    check_bool (trexp e1) i;
+    check_bool (trexp e2) i;
+    { exp = (); ty = Ast.Bool }
+  | Bin(e1, Or, e2, i) ->
+    check_bool (trexp e1) i;
+    check_bool (trexp e2) i;
+    { exp = (); ty = Ast.Bool }
+  | Bin(e1, Eq, e2, i) -> 
+    let { exp = e1; ty = ty1 } = trexp e1 in 
+    let { exp = e2; ty = ty2 } = trexp e2 in 
+      (match ty1 = ty2 with 
+       | false -> raise (Ill_Typed i)
+       | _ -> { exp = (); ty = Ast.Bool })
+  | Un(_, e, i) -> 
+    check_bool (trexp e) i;
+    { exp = (); ty = Ast.Bool }
+  | App(id, el, i) -> 
+    (try match lookup id venv with 
+         | Env.Var(ty) -> raise (Not_Function i)
+         | Env.Func(tyl, ty) -> 
+           (try List.iter2 
+                (fun e ty -> let { exp = e; ty = ty'} = trexp e in 
+                if not (ty = ty') then raise (Ill_Typed i) else ()) 
+                el tyl
+            with Invalid_argument(_) -> raise (Arity_Mismatched i)); { exp = (); ty = ty }
+    with Not_found -> assert false)
+  | ArrayAlloc(ty, e, i) -> 
+    check_int (trexp e) i;
+    { exp = (); ty = Ast.ArrayTy(ty) }
+  | Alloc(ty, i) -> 
+    (match ty with 
+    | NameTy(id) as ty -> (try let _ = lookup id glb_senv in 
+                         { exp = (); ty = ty } 
+                    with Not_found -> raise (Lack_Definition i) )
+    | _ -> raise (Alloc_Non_Struct i))
+  | Nil -> { exp = (); ty = Ast.Void }
+  | Var(var) -> trvar var 
+  and trvar = function 
+    | SimpVar(id, i) -> 
+      (match lookup id venv with 
+      | Env.Var(ty) -> { exp = (); ty = ty }
+      | _ -> assert false)
+    | FieldVar(var, fname, i) -> 
+      let { exp = e; ty = ty } = trvar var in 
+      (match ty with 
+      | NameTy(id) -> let tbl = lookup id glb_senv in 
+                      (match Hashtbl.find_opt tbl fname with 
+                      | Some(ty) -> { exp = e; ty = ty }
+                      | None -> raise (No_Fieldname i))
+      | _ -> raise (Not_Struct i))
+    | SubscriptVar(var, e, i) -> 
+      let { exp = e; ty = ty } = trvar var in 
+      (match ty with 
+      | ArrayTy(ty) -> { exp = (); ty = ty }
+      | _ -> raise (Ill_Typed i))
+  in 
+  fun e -> trexp e
+
+let rec trans_stmt : var_env -> str_env -> Ast.stmt -> Translate.exp = 
+  fun venv glb_senv -> 
+  let rec trvar = function 
+    | SimpVar(id, i) -> 
+      (match lookup id venv with 
+      | Env.Var(ty) -> { exp = (); ty = ty }
+      | _ -> assert false)
+    | FieldVar(var, fname, i) -> 
+      let { exp = e; ty = ty} = trvar var in 
+      (match ty with 
+      | NameTy(id) -> let tbl = lookup id glb_senv in 
+                      (match Hashtbl.find_opt tbl fname with 
+                      | Some(ty) -> { exp = e; ty = ty }
+                      | None -> raise (No_Fieldname i))
+      | _ -> raise (Not_Struct i))
+    | SubscriptVar(var, e, i) -> 
+      let { exp = e; ty = ty } = trvar var in 
+      { exp = (); ty = Ast.ArrayTy(ty)} in 
+  let rec trstmt = function 
+  | Assign(var, e, i) -> 
+    let { exp ; ty = ty} = trvar var in 
+    let { exp = exp'; ty = ty' } = trans_exp venv glb_senv e in 
+    (match ty = ty' with 
+    | true -> () 
+    | _ -> raise (Ill_Typed i))
+  | If(e, s, Some(s'), i) -> 
+    let { exp; ty } = trans_exp venv glb_senv e in 
+    (match ty with 
+    | Ast.Bool -> let () = trstmt s in trstmt s'
+    | _ -> raise (Ill_Typed i))
+  | If(e, s, None, i) -> 
+    let { exp; ty } = trans_exp venv glb_senv e in 
+    (match ty with 
+    | Ast.Bool -> trstmt s 
+    | _ -> raise (Ill_Typed i))
+  | While(e, s, i) -> 
+    let { exp; ty } = trans_exp venv glb_senv e in 
+    (match ty with 
+    | Ast.Bool -> trstmt s 
+    | _ -> raise (Ill_Typed i))
+  | Return(e, i) ->
+    let { exp = _; ty = _ } = trans_exp venv glb_senv e in 
+    () 
+  | Nop -> () 
+  | Exp(e, i) -> 
+    let { exp; ty = _ } = trans_exp venv glb_senv e in 
+    exp 
+  | Seq(sl, i) -> List.iter trstmt sl 
+  | Vardecl(id, t, s, i) -> 
+    trans_stmt (enter id (Env.Var(t)) venv) glb_senv s 
+  | Fundecl(id, Arrow(tyl, ty), s, i) -> 
+    trans_stmt (enter id (Env.Func(tyl, ty)) venv) glb_senv s
+  | Fundefn(id, idl, Arrow(tyl, ty), s', s, i) -> 
+    let venv' = (enter id (Env.Func(tyl, ty)) venv) in 
+    let venv'' = (try List.fold_left2 
+                      (fun acc id ty -> enter id (Env.Var(ty)) acc) venv' idl tyl
+                  with Invalid_argument _ -> assert false) in 
+      let _ = trans_stmt venv'' glb_senv s' in 
+      trans_stmt venv' glb_senv s
+  | Structdecl(_, s, _) -> 
+      trstmt s 
+  | Structdefn(_, _, s, _) -> 
+      trstmt s 
+  | _ -> () in 
+  fun s -> trstmt s
+
+
 
 let check s = 
-  check_def empty s;
+  let glb_senv = (check_def empty s) in 
   check_init s;
-  check_type empty s
+  trans_stmt empty glb_senv s
