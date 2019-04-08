0a91212c1f7e7ae2150f7de2154857c23696d7a6
KomaEc <31341411+KomaEc@users.noreply.github.com>
2019-04-08 12:46:16 +0800

    Add intermediate representation

--- ir_3addr.ml
+++ ir_3addr.ml
@@ -40,7 +40,7 @@ and binop = Plus | Minus | Times | Div
 
 and relop = Eq | Lt | And | Or
 
-let print_header = print_string "  "
+let print_header () = print_string "  "
 
 let string_of_bop = function 
   | Plus -> " + "
@@ -56,72 +56,72 @@ let string_of_rop = function
 
 let rec print_stmt = function 
   | Assign_var(t1, t2) -> 
-    print_header;
+    print_header();
     print_endline (string_of_temp t1 ^ " = " ^ string_of_temp t2 ^ ";")
   | Assign_const(t, i) -> 
-    print_header;
+    print_header();
     print_endline (string_of_temp t ^ " = " ^ string_of_int i ^ ";")
   | Assign_label(t, l) -> 
-    print_header;
+    print_header();
     print_endline (string_of_temp t ^ " = " ^ string_of_label l ^ ";") 
   | Arith(t1, t2, bop, t3) -> 
-    print_header;
+    print_header();
     print_endline (string_of_temp t1 ^ " = " 
                   ^ string_of_temp t2 ^ string_of_bop bop 
                   ^ string_of_temp t3 ^ ";")
   | Rel(t1, t2, rop, t3) -> 
-    print_header;
+    print_header();
     print_endline (string_of_temp t1 ^ " = " 
                   ^ string_of_temp t2 ^ string_of_rop rop 
                   ^ string_of_temp t3 ^ ";")
   | Label(l) -> 
     print_endline (string_of_label l ^ ":")
   | Jmp(l) -> 
-    print_header;
+    print_header();
     print_endline ("goto " ^ string_of_label l ^ ";")
   | CJmp(t, l) -> 
-    print_header;
+    print_header();
     print_endline ("ifZ " ^ string_of_temp t ^ " goto "
                   ^ string_of_label l ^ ";")
   | Push(t) -> 
-    print_header;
+    print_header();
     print_endline ("push_param " ^ string_of_temp t ^ ";")
   | Pop(t) -> 
-    print_header;
+    print_header();
     print_endline ("pop_param " ^ string_of_temp t ^ ";")
   | Fun_call_void(l) ->
-    print_header;
+    print_header();
     print_endline ("LCall " ^ string_of_label l ^ ";") 
   | Fun_call_ret(l, t) -> 
-    print_header;
+    print_header();
     print_endline (string_of_temp t ^ " = " 
                   ^ "LCall " ^ string_of_label l ^ ";")
   | Fun_begin(i) -> 
-    print_header;
+    print_header();
     print_endline ("begin_func " ^ string_of_int i ^ ";")
   | Fun_end -> 
-    print_header;
+    print_header();
     print_endline ("end_func;")
   | Ret_val(t) -> 
-    print_header;
+    print_header();
     print_endline ("return " ^ string_of_temp t ^ ";") 
   | Ret_void -> 
-    print_header;
+    print_header();
     print_endline "return;"
   | Fetch_simp(t1, t2) -> 
-    print_header;
+    print_header();
     print_endline (string_of_temp t1 ^ " = *(" ^ string_of_temp t2 
                   ^ ");")
   | Fetch_comp(t1, t2, i) -> 
-    print_header;
+    print_header();
     print_endline (string_of_temp t1 ^ " = *(" ^ string_of_temp t2 
                   ^ " + " ^ string_of_int i ^ ");") 
   | Store_simp(t1, t2) -> 
-    print_header;
+    print_header();
     print_endline ("*(" ^ string_of_temp t1 ^ ") = "
                   ^ string_of_temp t2 ^ ";")
  | Store_comp(t1, i, t2) -> 
-    print_header;
+    print_header();
     print_endline ("*(" ^ string_of_temp t1 ^ " + " ^ string_of_int i
                   ^ ") = " ^ string_of_temp t2 ^ ";")
 
